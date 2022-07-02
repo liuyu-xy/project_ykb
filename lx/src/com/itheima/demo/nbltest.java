2ac3ddc519b61843ebd44241cb32b56914cac03e
xiaoyu <3013651711@qq.com>
2022-07-02 16:05:21 +0800

    1

--- lx/src/com/itheima/demo/nbltest.java
+++ lx/src/com/itheima/demo/nbltest.java
@@ -2,9 +2,9 @@ package com.itheima.demo;
 public class nbltest {
         public static void main(String[] args) {
    /*         nbltest.nbl in = new nbltest().new nbl();*/
-            nbldemo.nbl in = new nbl();
-            in.show();
-            System.out.println(in.a);
+           // nbldemo.nbl in = new nbl();
+/*            in.show();
+            System.out.println(in.a);*/
         }
 
 
